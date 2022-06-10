0486851cec7c92e4c26ff6840b6cb72d025550d1
juanib12 <bjuan.ignacio11@gmail.com>
2022-06-10 18:21:54 -0300

    new

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
+const Clientes = require("../models/Clientes");
 require("dotenv").config();
 
 mongoose.connect(
@@ -38,13 +39,18 @@ app.use(cors(corsOptions));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/clientes", Cliente.list);
+//CLIENTES
+app.get("/clientes", (req, res) => {
+  const clientes = await Clientes.find();
+  res.status(200).send(clientes);
+});
 app.get("/clientes/:dni", Cliente.get);
 app.post("/clientes", Cliente.create);
 app.put("/clientes/:id", Cliente.update);
 app.patch("/clientes/:id", Cliente.update);
 app.delete("/clientes/:id", Cliente.destroy);
 
+//ORDENES
 app.get("/ordenes", Orden.list);
 app.get("/ordenes/:id", Orden.get);
 app.post("/ordenes", Orden.create);
@@ -52,6 +58,7 @@ app.put("/ordenes/:id", Orden.update);
 app.patch("/ordenes/:id", Orden.update);
 app.delete("/ordenes/:id", Orden.destroy);
 
+//REPUESTOS
 app.get("/repuestos", Repuesto.list);
 app.get("/repuestos/:id", Repuesto.get);
 app.post("/repuestos", Repuesto.create);
@@ -59,6 +66,8 @@ app.put("/repuestos/:id", Repuesto.update);
 app.patch("/repuestos/:id", Repuesto.update);
 app.delete("/repuestos/:id", Repuesto.destroy);
 
+
+//EQUIPOS
 app.get("/equipos", Equipo.list);
 app.get("/equipos/:nro_serie", Equipo.get);
 app.post("/equipos", Equipo.create);
@@ -66,9 +75,12 @@ app.put("/equipos/:id", Equipo.update);
 app.patch("/equipos/:id", Equipo.update);
 app.delete("/equipos/:id", Equipo.destroy);
 
+//BUSCAr
 app.get("/buscarorden/:nro_orden", BuscarOrden.get);
 app.get("/buscarcliente/:dni", BuscarCliente.get);
 
+
+//REPUESTOS ORDN
 app.get("/repuestosorden", RepuestosOrden.list);
 app.get("/repuestosorden/:nro_orden", RepuestosOrden.get);
 app.post("/repuestosorden", RepuestosOrden.create);
