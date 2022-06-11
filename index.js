e8bc978b6cd27738b85b3001ab012c05fe919120
juanib12 <bjuan.ignacio11@gmail.com>
2022-06-11 08:59:34 -0300

    new clinteha

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 const corsOptions = {
-  origin: process.env.ORIGIN,
+  origin: ["https://techfix-demo.vercel.app", "https://techfix-app.vercel.app"],
   credentials: true,
 };
 
